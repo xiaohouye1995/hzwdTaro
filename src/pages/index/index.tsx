1a4866488c14bef3e1126c0171c5a7861f38a0cb
zhoujunyang <363993481@qq.com>
2022-06-14 19:22:04 +0800

    feat: 初始化

---
+++ src/pages/index/index.tsx
@@ -0,0 +1,22 @@
+import { Component } from 'react'
+import { View, Text } from '@tarojs/components'
+import './index.scss'
+
+export default class Index extends Component {
+
+  componentDidMount () { }
+
+  componentWillUnmount () { }
+
+  componentDidShow () { }
+
+  componentDidHide () { }
+
+  render () {
+    return (
+      <View className='index'>
+        <Text>杭州网达软件跨端应用</Text>
+      </View>
+    )
+  }
+}
